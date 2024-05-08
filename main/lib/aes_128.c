6c8f0e621cca6842f332c79d95795b0eac786b90
bramalka <bramandha27@gmail.com>
2024-05-08 10:15:31 +0700

    deleted unnecessary command

--- main/lib/aes_128.c
+++ main/lib/aes_128.c
@@ -3,33 +3,20 @@
 #include <stdio.h>
 
 
-
 void encrypt_data(uint8_t *data, size_t data_len, uint8_t *key, size_t key_len, uint8_t *encrypted_data) {
     if (key_len != AES_128_KEY_BYTES) {
         printf("Error: Wrong key size\n");
         return;
     }
 
-    // printf("Raw Data:\n");
-    // for (int i = 0; i < data_len; i++) {
-    //     printf("%02x ", data[i]);
-    // }
-    // printf("\n");
-
     esp_aes_context aes_ctx;
     esp_aes_init(&aes_ctx);
 
     esp_aes_setkey(&aes_ctx, key, key_len * 8);
 
-    // printf("Encrypted Data:\n");   
-    for (int i = 0; i < 64; i += 16) {
+    for (int i = 0; i < data_len; i += 16) {
         esp_aes_crypt_ecb(&aes_ctx, ESP_AES_ENCRYPT, &data[i], &encrypted_data[i]);
     }
-
-    // for (int i = 0; i < 64; i++) {
-    //     printf("%02x ", encrypted_data[i]);
-    // }
-    // printf("\n");
 
     esp_aes_free(&aes_ctx);
 }
@@ -45,15 +32,9 @@ void decrypt_data(uint8_t *data, size_t data_len, uint8_t *key, size_t key_len, 
 
     esp_aes_setkey(&aes_ctx, key, key_len * 8);
 
-    // printf("Decrypted Data:\n");
-    for (int i = 0; i < 64; i += 16) {
+    for (int i = 0; i < data_len; i += 16) {
         esp_aes_crypt_ecb(&aes_ctx, ESP_AES_DECRYPT, &data[i], &decrypted_data[i]);
     }    
          
-    // for (int i = 0; i < 64; i++) {
-    //     printf("%02x ", data[i]);
-    // }
-    // printf("\n");
-
     esp_aes_free(&aes_ctx);
 }
